d568980950d176c70fe991630d1610b5225bd12a
kauanmateuss <kamateus77@gmail.com>
2025-07-11 13:42:43 -0300

    Criando tabela que exibe os contatos na página

--- contact/views/contact_views.py
+++ contact/views/contact_views.py
@@ -1,8 +1,16 @@
 from django.shortcuts import render
+from contact.models import Contact
 
 # Create your views here.
 def home(request):
+    contacts = Contact.objects.all()
+
+    context = {
+        "contacts": contacts,
+    }
+
     return render(
         request,
-        'global/base.html'
+        'contact/index.html',
+        context,
     )
